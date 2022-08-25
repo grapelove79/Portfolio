cb4460319eac14f7e9cc3062fb70bf37bcabb543
Kim Youngsun <grapelove79@gmail.com>
2022-08-26 01:56:47 +0900

    CSS: About

--- main.js
+++ main.js
@@ -29,6 +29,12 @@ const colorFn = (i, classname, color) => {
   classname.style.background = "conic-gradient(" + color + " 0% " + i + "%, #dedede " + i + "% 100%)";
 }
 
+const replay = () => {
+  makeChart(80, chart1, '#f5b914');
+  makeChart(40, chart2, '#0A174E');
+  makeChart(60, chart3, '#66d2ce');
+  makeChart(60, chart4, '#dd8acb');
+}
 
 makeChart(80, chart1, '#f5b914');
 makeChart(40, chart2, '#0A174E');
